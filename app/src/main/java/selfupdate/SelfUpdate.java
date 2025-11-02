238668a9f89891a05278fc69840f41d282dc3ed7
Nikhil Sethi <sethi.nirvil@gmail.com>
2025-11-02 22:25:53 +0100

    Add self update src code

---
+++ app/src/main/java/selfupdate/SelfUpdate.java
@@ -0,0 +1,188 @@
+package selfupdate;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.ComponentActivity;
+
+import androidx.core.content.FileProvider;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import com.example.kijk.BuildConfig;
+import com.example.kijk.R;
+import selfupdate.github.Release;
+import selfupdate.github.Asset;
+import okhttp3.Interceptor;
+import okhttp3.Response;
+
+import android.util.Log;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class SelfUpdate {
+
+    private static final String FILENAME_APK = "update.apk";
+    private static final String CONTENT_TYPE_APK = "application/vnd.android.package-archive";
+    private static final String BASE_URL = "https://api.github.com/repos/%s/%s/releases/tags/0.0.1";
+
+    private static AppCompatActivity activity;
+    private static SelfUpdateHttpClient httpClient;
+
+
+    private static boolean isSemverGreater(String a, String b){
+        // split strings into major, minor, patch
+        String regex = "[.]";
+        String[] aArr = a.split(regex);
+        String[] bArr = b.split(regex);
+        Log.d("version release: ", Arrays.toString(aArr));
+        Log.d("version current: ", Arrays.toString(bArr));
+
+        // compare ints
+        boolean minorGreater = Integer.parseInt(aArr[1])>Integer.parseInt(bArr[1]);
+        boolean patchGreater = Integer.parseInt(aArr[2])>Integer.parseInt(bArr[2]);
+        boolean majorGreater = Integer.parseInt(aArr[0])>Integer.parseInt(bArr[0]);
+
+        return  (majorGreater || minorGreater) || patchGreater;
+    }
+    public static void checkUpdate(AppCompatActivity activity, String username, String repository) {
+        final String url = String.format(BASE_URL, username, repository);
+        SelfUpdate.activity = activity;
+        httpClient = new SelfUpdateHttpClient(activity);
+
+        if (!httpClient.isConnectedToInternet()) return;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Release release = null;
+                try {
+                    release = httpClient.getLastRelease(url);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (release != null) {
+                    try {
+                        if (isSemverGreater(release.getTag_name(), BuildConfig.VERSION_NAME))
+                            showUpdateAvailable(release);
+                    } catch (NumberFormatException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private static void showUpdateAvailable(final Release release) {
+        String downloadUrl = null;
+        for (Asset asset : release.getAssets()) {
+            if (asset.getContent_type().equals(CONTENT_TYPE_APK)) {
+                downloadUrl = asset.getBrowser_download_url();
+                break;
+            }
+        }
+        if (downloadUrl == null) return;
+
+        final String finalDownloadUrl = downloadUrl;
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                View content = activity.getLayoutInflater().inflate(R.layout.content_dialog_show_update, null);
+                TextView version = content.findViewById(R.id.version);
+                version.setText(String.format("%s :", release.getName()));
+                TextView changelog = content.findViewById(R.id.changelog);
+                changelog.setText(release.getBody());
+
+                new AlertDialog.Builder(activity)
+                        .setTitle(activity.getResources().getString(R.string.dialog_update_title))
+                        .setView(content)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                updateApp(finalDownloadUrl);
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, null)
+                        .show();
+            }
+        });
+    }
+
+    private static void updateApp(String downloadUrl) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            downloadUpdate(downloadUrl);
+        } else {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(downloadUrl));
+            activity.startActivity(browserIntent);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private static void downloadUpdate(final String url) {
+        View content = activity.getLayoutInflater().inflate(R.layout.content_dialog_download, null);
+
+        final AlertDialog alertDialog = new AlertDialog.Builder(activity)
+                .setTitle(activity.getResources().getString(R.string.dialog_downloading_title))
+                .setView(content)
+                .setCancelable(false)
+                .show();
+
+        final ProgressBar progressBar = content.findViewById(R.id.progressBar);
+        final ProgressListener progressListener = new ProgressListener() {
+            @Override
+            public void update(long bytesRead, long contentLength, boolean done) {
+                final int progress = Math.round(((float) bytesRead / contentLength) * 100);
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (!activity.isDestroyed())
+                            progressBar.setProgress(progress);
+                    }
+                });
+                if (done && !activity.isDestroyed()) alertDialog.dismiss();
+            }
+        };
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Interceptor interceptor = new Interceptor() {
+                    @NotNull
+                    @Override
+                    public Response intercept(@NotNull Chain chain) throws IOException {
+                        Response originalResponse = chain.proceed(chain.request());
+                        return originalResponse.newBuilder()
+                                .body(new ProgressResponseBody(originalResponse.body(), progressListener))
+                                .build();
+                    }
+                };
+
+                try {
+                    File apkFile = httpClient.download(url, FILENAME_APK, interceptor);
+                    if (apkFile != null) installAPK(apkFile);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private static void installAPK(File apkFile) {
+        Uri uri = FileProvider.getUriForFile(activity, activity.getPackageName() + ".provider", apkFile);
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setDataAndType(uri, CONTENT_TYPE_APK);
+        i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        activity.startActivity(i);
+    }
+}
---
+++ app/src/main/java/selfupdate/SelfUpdateHttpClient.java
@@ -0,0 +1,142 @@
+package selfupdate;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.UnknownHostException;
+
+import selfupdate.github.Release;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okio.BufferedSink;
+import okio.Okio;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class SelfUpdateHttpClient {
+
+    private OkHttpClient simpleOkHttpClient;
+    private final Context context;
+
+    public SelfUpdateHttpClient(Context context) {
+        this.context = context;
+        simpleOkHttpClient = new OkHttpClient();
+    }
+
+    public Release getLastRelease(String url) throws HttpException, IOException, NoInternetConnectionException {
+        String body = get(url);
+        Moshi moshi = new Moshi.Builder().build();
+        JsonAdapter<Release> jsonAdapter = moshi.adapter(Release.class);
+        return jsonAdapter.fromJson(body);
+    }
+
+    public File download(String url, String filename, Interceptor networkInterceptor) throws HttpException, UnknownHostException, NoInternetConnectionException, InterruptedIOException {
+        if (!isConnectedToInternet()) throw new NoInternetConnectionException();
+
+        OkHttpClient okHttpClient = simpleOkHttpClient;
+        if (networkInterceptor != null)
+            okHttpClient = new OkHttpClient.Builder().addNetworkInterceptor(networkInterceptor).build();
+
+        Request request = new Request.Builder().url(url).build();
+
+        Response response = null;
+        try {
+            response = okHttpClient.newCall(request).execute();
+        } catch (IOException e) {
+            if (e.getClass() == UnknownHostException.class)
+                throw (UnknownHostException) e;
+            else if (e.getClass() == InterruptedIOException.class)
+                throw (InterruptedIOException) e;
+            e.printStackTrace();
+        }
+
+        if (!response.isSuccessful()) {
+            throw new HttpException(response.code());
+        }
+
+        ResponseBody responseBody = response.body();
+        if (responseBody == null) return null;
+
+        try {
+            File file = new java.io.File((context.getApplicationContext().getFileStreamPath(filename).getPath()));
+            BufferedSink sink = Okio.buffer(Okio.sink(file));
+            sink.writeAll(response.body().source());
+            sink.close();
+            return file;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private String get(String url) throws HttpException, UnknownHostException, NoInternetConnectionException, InterruptedIOException {
+        if (!isConnectedToInternet()) throw new NoInternetConnectionException();
+
+        Request request = new Request.Builder().url(url).build();
+
+        Response response = null;
+        try {
+            response = simpleOkHttpClient.newCall(request).execute();
+        } catch (IOException e) {
+            if (e.getClass() == UnknownHostException.class)
+                throw (UnknownHostException) e;
+            else if (e.getClass() == InterruptedIOException.class)
+                throw (InterruptedIOException) e;
+            e.printStackTrace();
+        }
+        Log.i("",url);
+        if (!response.isSuccessful()) throw new HttpException(response.code());
+
+        ResponseBody responseBody = response.body();
+        if (responseBody == null) return null;
+
+        try {
+            return responseBody.string();
+        } catch (IOException e) {
+            // nothing
+        }
+        return null;
+    }
+
+    public boolean isConnectedToInternet() {
+        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            for (NetworkInfo networkInfo : info)
+                if (networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    public static class NoInternetConnectionException extends Exception {
+    }
+
+    public static class HttpException extends Exception {
+        private final int code;
+
+        public HttpException(int code) {
+            super("HTTP Exception " + code);
+            this.code = code;
+        }
+
+        public int getCode() {
+            return code;
+        }
+    }
+}
