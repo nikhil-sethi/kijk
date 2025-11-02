728417b7979d560c993d747f062c0cb49417738d
Nikhil Sethi <sethi.nirvil@gmail.com>
2025-11-02 22:26:35 +0100

    Add self update calls and resources

--- app/src/main/java/com/example/kijk/MainActivity.kt
+++ app/src/main/java/com/example/kijk/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.kijk
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
+//import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+import androidx.activity.enableEdgeToEdge
+import selfupdate.SelfUpdate
+import android.util.Log
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        Log.d("CREATION", "and then the world was created")
+        SelfUpdate.checkUpdate(this, "nikhil-sethi", "kijk")
     }
 }
